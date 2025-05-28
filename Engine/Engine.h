0ee67c57f5c15e93565987a0b9bd19e84fe26809
aliosmansahin <aliosmsahin01@gmail.com>
2025-05-28 19:04:21 +0300

    Some changes

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -57,6 +57,8 @@ public:
 	std::string projectName = "project";
 	//std::string projectPath = "C:\\Users\\osman\\OneDrive\\Desktop\\"; This is for mine
 	std::string projectPath = "";//TODO: Change it to yours
+
+	//Test for singing commits, ignore this
 	
 	//store screen width and height
 	int screenWidth = 0;
