feeea8980f77edec5be24cd9fe8ce09d37069952
aliosmansahin <aliosmsahin01@gmail.com>
2025-05-28 19:25:53 +0300

    singing settings done

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -55,11 +55,9 @@ private:
 public:
 	//project specifications
 	std::string projectName = "project";
-	//std::string projectPath = "C:\\Users\\osman\\OneDrive\\Desktop\\"; This is for mine
-	std::string projectPath = "";//TODO: Change it to yours
+	std::string projectPath = "C:\\Users\\osman\\OneDrive\\Desktop\\"; This is for mine
+	//std::string projectPath = "";//TODO: Change it to yours
 
-	//Test for singing commits, ignore this,,,,,
-	
 	//store screen width and height
 	int screenWidth = 0;
 	int screenHeight = 0;
