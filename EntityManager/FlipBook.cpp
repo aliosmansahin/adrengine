708c8a7b0349576600c2533a87b215b4c1fe6b16
aliosmansahin <aliosmsahin01@gmail.com>
2025-06-08 13:00:16 +0300

    flipbook edit
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- EntityManager/FlipBook.cpp
+++ EntityManager/FlipBook.cpp
@@ -29,7 +29,7 @@ PURPOSE: Updates the entity
 */
 ENTITYMANAGER_API void FlipBook::Update()
 {
-    
+	
 }
 
 /*
@@ -40,9 +40,100 @@ ENTITYMANAGER_API void FlipBook::Draw(glm::vec3 currentSceneCameraPos)
 {
 }
 
+/*
+PURPOSE: Creates each frame and starts updating it
+*/
+ENTITYMANAGER_API void FlipBook::CreateFrames(float textureWidth, float textureHeight, float frameWidth, float frameHeight)
+{
+	//Clear previous
+	createdFrames.clear();
+
+	//Save framesize
+	this->frameWidth = (int)frameWidth;
+	this->frameHeight = (int)frameHeight;
+
+	//Calculate frame count
+	int frameCountX = (int)std::ceil(textureWidth / frameWidth);
+	int frameCountY = (int)std::ceil(textureHeight / frameHeight);
+
+	for (int y = 0; y < frameCountY; ++y) {
+		/*
+			These calculations are for checking if some of frames don't have decided size.
+			It they don't we will give them as most size as possible,
+			In this scenario, we will give them the last texture size.
+		*/
+		//pass tile height into a new variable
+		int realTileHeight = (int)frameHeight;
+
+		//if heights of the tiles aren't equal to each other
+		if ((int)textureHeight % (int)frameHeight != 0) {
+			//if this tile is the last in y coord in the tilemap
+			if (y == frameCountY - 1) {
+				//calculate y position of the tile in the tilemap and sub it from tilemap width
+				int currentPosYInTileMap = y * (int)frameHeight;
+				int lastTextureSizeY = (int)textureHeight - currentPosYInTileMap;
+
+				//pass the new width
+				realTileHeight = lastTextureSizeY;
+			}
+		}
+
+		for (int x = 0; x < frameCountX; ++x) {
+			//pass tile width into a new variable
+			int realTileWidth = (int)frameWidth;
+
+			//if widths of the tiles aren't equal to each other
+			if ((int)textureWidth % (int)frameWidth != 0) {
+
+				//if this tile is the last in x coord in the tilemap
+				if (x == frameCountX - 1) {
+					//calculate x position of the tile in the tilemap and sub it from tilemap width
+					int currentPosXInTileMap = x * (int)frameWidth;
+					int lastTextureSizeX = (int)textureWidth - currentPosXInTileMap;
+
+					//pass the new width
+					realTileWidth = lastTextureSizeX;
+				}
+			}
+
+			//Create tile and insert it into createdTiles
+			FlipBookFrame* frame = new FlipBookFrame();
+			float texturePosX = x * frameWidth;
+			float texturePosY = y * frameHeight;
+
+			/*
+				Here we are translating pixel coordinates to[0.0, 1.0].
+				For example, the texture width is 512 pixels, and the current tile pos is 256,
+				After translation, the new x value will be 0.5
+			*/
+			float u = texturePosX * 1.0f / textureWidth;
+			float v = texturePosY * 1.0f / textureHeight;
+
+			//TT* stands for "translated tile width/height" btw
+			float TTX = realTileWidth * 1.0f / textureWidth;
+			float TTY = realTileHeight * 1.0f / textureHeight;
+
+			frame->width = realTileWidth;
+			frame->height = realTileHeight;
+			frame->u = u;
+			frame->v = v;
+			frame->textureWidth = TTX;
+			frame->textureHeight = TTY;
+			frame->x = x;
+			frame->y = y;
+
+			createdFrames.push_back({ true, std::shared_ptr<FlipBookFrame>(frame) });
+		}
+	}
+}
+
+/*
+PURPOSE: Creates a framebuffer and other buffer objects for the inspector
+*/
 ENTITYMANAGER_API void FlipBook::CreateInspectFrameBuffer(float textureWidth, float textureHeight, int frameWidth, int frameHeight)
 {
     //------ RELEASING ------
+	createdFrames.clear();
 	
 	//Delete old Buffers
 	if (inspectFrameBuffer != -1) {
@@ -57,6 +148,59 @@ ENTITYMANAGER_API void FlipBook::CreateInspectFrameBuffer(float textureWidth, fl
 		adr_glDeleteTextures(1, &inspectTexture);
 		inspectTexture = -1;
 	}
+	if (VAO != -1) {
+		adr_glDeleteVertexArrays(1, &VAO);
+		VAO = -1;
+	}
+	if (VBO != -1) {
+		adr_glDeleteBuffers(1, &VBO);
+		VBO = -1;
+	}
+	if (EBO != -1) {
+		adr_glDeleteBuffers(1, &EBO);
+		EBO = -1;
+	}
+
+	//------ TEXTURE FLIPBOOK ------
+
+	//Vertices for the texture
+	float vertices[] = {
+		//X					Y		   Z	 U     V
+		 textureWidth, textureHeight, 0.0f, 1.0f, 0.0f, // top right
+		 0.0f,	       textureHeight, 0.0f, 0.0f, 0.0f, // bottom right
+		 0.0f,         0.0f,          0.0f, 0.0f, 1.0f, // bottom left
+		 textureWidth, 0.0f,          0.0f, 1.0f, 1.0f, // top left 
+	};
+	//Indices for the texture
+	unsigned int indices[] = {
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+	//Create buffers and bind them with vertices and indices
+	adr_glGenBuffers(1, &VBO);
+	adr_glGenBuffers(1, &EBO);
+
+	adr_glGenVertexArrays(1, &VAO);
+	adr_glBindVertexArray(VAO);
+
+	adr_glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	adr_glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	adr_glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	adr_glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Set the vertex attrib pointers (position = 0)
+	adr_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	adr_glEnableVertexAttribArray(0);
+
+	//Set the vertex attrib pointers (position = 1)
+	adr_glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	adr_glEnableVertexAttribArray(1);
+
+	//Release buffers
+	adr_glBindBuffer(GL_ARRAY_BUFFER, 0);
+	adr_glBindVertexArray(0);
 
 	//------ FRAME BUFFER ------
 
@@ -96,7 +240,157 @@ ENTITYMANAGER_API bool FlipBook::IsInspectCreated()
 		return false;
 	if (inspectTexture == -1)
 		return false;
+	if (VAO == -1)
+		return false;
+	if (VBO == -1)
+		return false;
+	if (EBO == -1)
+		return false;
 	return true;
+}
+
+/*
+PURPOSE: Updates the inspector, its empty for now
+*/
+ENTITYMANAGER_API void FlipBook::UpdateInspect()
+{
+}
+
+/*
+PURPOSE: Renders the inspector to the framebuffer
+*/
+ENTITYMANAGER_API void FlipBook::DrawInspect(int width, int height, int frameW, int frameH)
+{
+	//Save the previous type of shader
+	Utils::ShaderType prevShaderType = ShaderManager::GetInstance().GetCurrentType();
+
+	//Change the shader to draw inspector
+	ShaderManager::GetInstance().UseShaders(Utils::SHADER_INSPECT_TILE);
+
+	//Set the framebuffer
+	adr_glBindFramebuffer(GL_FRAMEBUFFER, inspectFrameBuffer);
+	Graphics::GetInstance().Clear();
+	adr_glViewport(0, 0, (GLsizei)(width), (GLsizei)(height));
+
+	//Some calculations for transformation
+	glm::mat4 model = glm::mat4(1.0f);
+	//model = glm::scale(model, glm::vec3(scale, scale, 1.0f));
+	glm::mat4 proj = glm::ortho(0.0f, (float)width, (float)height, 0.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+
+	//Send the transformation matrix to the shader
+	ShaderManager::GetInstance().ApplyTransformMatrix("uModel", model);
+	ShaderManager::GetInstance().ApplyTransformMatrix("uProjection", proj);
+	ShaderManager::GetInstance().ApplyTransformMatrix("uView", view);
+	ShaderManager::GetInstance().ApplyUniformInt("tileW", frameW);
+	ShaderManager::GetInstance().ApplyUniformInt("tileH", frameH);
+	ShaderManager::GetInstance().ApplyUniformInt("textureWidth", width);
+	ShaderManager::GetInstance().ApplyUniformInt("textureHeight", height);
+
+	//Set the texture
+	adr_glActiveTexture(GL_TEXTURE0);
+	adr_glBindTexture(GL_TEXTURE_2D, params->texture);
+	ShaderManager::GetInstance().ApplyTexture("texture1");
+
+	//Draw the texture
+	adr_glBindVertexArray(VAO);
+	adr_glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	adr_glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	//Disable after drawing
+	adr_glActiveTexture(GL_TEXTURE0);
+	adr_glBindTexture(GL_TEXTURE_2D, 0);
+	adr_glBindVertexArray(0);
+
+	ShaderManager::GetInstance().UseShaders(prevShaderType);
+}
+
+/*
+PURPOSE: Gets the texture of the inspector
+*/
+ENTITYMANAGER_API unsigned int FlipBook::GetInspectTexture()
+{
+	return inspectTexture;
+}
+
+/*
+PURPOSE: Returns created frames
+*/
+ENTITYMANAGER_API std::vector<std::pair<bool, std::shared_ptr<FlipBookFrame>>>& FlipBook::GetCreatedFrames()
+{
+	return createdFrames;
+}
+
+/*
+PURPOSE: Creates buffers, a new "frames" vector for storing each frame that is selected and starts updating it
+*/
+ENTITYMANAGER_API void FlipBook::StartFlipBook()
+{
+	if (frameVAO != -1) {
+		adr_glDeleteVertexArrays(1, &frameVAO);
+		frameVAO = -1;
+	}
+	if (frameVBO != -1) {
+		adr_glDeleteBuffers(1, &frameVBO);
+		frameVBO = -1;
+	}
+	if (frameEBO != -1) {
+		adr_glDeleteBuffers(1, &frameEBO);
+		frameEBO = -1;
+	}
+
+	//Store selected frames into another vector
+	for (auto& frame : createdFrames) {
+		if (!frame.first)
+			continue;
+
+		frames.push_back(frame.second);
+	}
+
+	//Set current index at 0
+	currentIndex = 0;
+
+	//------ TEXTURE FRAME ------
+
+	//Vertices for the texture
+	float vertices[] = {
+		//X					Y	   Z	 U     V
+		 frameWidth, frameHeight, 0.0f, 1.0f, 0.0f, // top right
+		 0.0f,	     frameHeight, 0.0f, 0.0f, 0.0f, // bottom right
+		 0.0f,       0.0f,        0.0f, 0.0f, 1.0f, // bottom left
+		 frameWidth, 0.0f,        0.0f, 1.0f, 1.0f, // top left 
+	};
+	//Indices for the texture
+	unsigned int indices[] = {
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+	//Create buffers and bind them with vertices and indices
+	adr_glGenBuffers(1, &frameVBO);
+	adr_glGenBuffers(1, &frameEBO);
+
+	adr_glGenVertexArrays(1, &frameVAO);
+	adr_glBindVertexArray(frameVAO);
+
+	adr_glBindBuffer(GL_ARRAY_BUFFER, frameVBO);
+	adr_glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	adr_glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, frameEBO);
+	adr_glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Set the vertex attrib pointers (position = 0)
+	adr_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	adr_glEnableVertexAttribArray(0);
+
+	//Set the vertex attrib pointers (position = 1)
+	adr_glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	adr_glEnableVertexAttribArray(1);
+
+	//Release buffers
+	adr_glBindBuffer(GL_ARRAY_BUFFER, 0);
+	adr_glBindVertexArray(0);
 }
 
 /*
--- InterfaceManager/WindowFlipBookEdit.cpp
+++ InterfaceManager/WindowFlipBookEdit.cpp
@@ -83,7 +83,120 @@ INTERFACEMANAGER_API void WindowFlipBookEdit::DrawWindow()
         }
 
         if (textureIter != textures.end() && edittingFlipBook->IsInspectCreated()) {
-            //TODO: Write preview codes
+            //Update and render the inspector to the framebuffer
+            edittingFlipBook->UpdateInspect();
+            edittingFlipBook->DrawInspect(textureWidth, textureHeight, frameWidth, frameHeight);
+
+            //Draw the inspector
+            ImGui::SeparatorText("Inspector");
+            ImVec2 pos = ImGui::GetCursorScreenPos();
+            ImVec2 windowPos = ImGui::GetCursorPos();
+            ImGui::GetWindowDrawList()->AddImage(
+                (ImTextureID)(intptr_t)edittingFlipBook->GetInspectTexture(),
+                ImVec2(pos.x, pos.y),
+                ImVec2(pos.x + textureWidth, pos.y + textureHeight),
+                ImVec2(0, 0),
+                ImVec2(1, 1)
+            );
+
+            //Some visuals
+            int padding = 10;
+            ImGui::SetCursorPos(ImVec2(windowPos.x, windowPos.y + textureHeight + padding));
+
+            //Write parameters
+            ImGui::Text(std::string("Texture Size: " + std::to_string(textureWidth) + "x" + std::to_string(textureHeight) + "px").c_str());
+            ImGui::Text(std::string("Tile Size: " + std::to_string(frameWidth) + "x" + std::to_string(frameHeight) + "px").c_str());
+
+            //Set the size of each tiles
+            ImGui::DragInt("Frame Width", &frameWidth);
+            ImGui::DragInt("Frame Height", &frameHeight);
+
+            if (ImGui::Button("Create Frames")) {
+                //Create each tile
+                edittingFlipBook->CreateFrames((float)textureWidth, (float)textureHeight, (float)frameWidth, (float)frameHeight);
+            }
+
+            auto& frames = edittingFlipBook->GetCreatedFrames();
+            if (!frames.empty()) {
+                ImGui::SeparatorText("Created Tiles");
+                ImVec2 windowPos = ImGui::GetCursorPos();
+                ImVec2 pos = ImGui::GetCursorScreenPos();
+                ImVec2 startPos = ImGui::GetCursorScreenPos();
+
+                for (int i = 0; i < frames.size(); ++i) {
+                    FlipBookFrame* frame = frames[i].second.get();
+
+                    pos = ImGui::GetCursorScreenPos();
+
+                    //Pass the coordinates
+                    int tileX = frame->x;
+                    int tileY = frame->y;
+                    int currentTileWidth = frame->width;
+                    int currentTileHeight = frame->height;
+                    float u = frame->u;
+                    float v = frame->v;
+                    float tw = frame->textureWidth;
+                    float th = frame->textureHeight;
+
+                    //Set the position of the preview of the tile
+                    pos.x += tileX * (frameWidth + padding);
+                    pos.y += tileY * (frameHeight + padding);
+
+                    //Image Button spec
+                    ImVec2 imageSize = ImVec2((float)edittingFlipBook->frameWidth, (float)edittingFlipBook->frameHeight);
+                    std::string imageButtonId = "##image_button" + std::to_string(i);
+
+                    //Calculate current button positions
+                    ImVec2 currentButtonPos;
+                    currentButtonPos.x = windowPos.x + tileX * (edittingFlipBook->frameWidth + padding);
+                    currentButtonPos.y = windowPos.y + tileY * (edittingFlipBook->frameHeight + padding);
+
+                    //Change color when its selected
+                    bool selected = frames[i].first;
+                    ImVec4 bgColor = ImVec4(0.2f, 0.2f, 0.5, 1.0f);
+                    if (selected)
+                        bgColor = ImVec4(0.0f, 0.7f, 0.0, 1.0f);
+
+                    //Set the position of the button
+                    ImGui::SetCursorPos(currentButtonPos);
+
+                    //Some color sets
+                    ImGui::PushStyleColor(ImGuiCol_Button, bgColor);
+                    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
+
+                    //Draw the tile
+                    if (ImGui::ImageButton(
+                        imageButtonId.c_str(),
+                        (ImTextureID)(intptr_t)params->texture,
+                        imageSize,
+                        ImVec2(u, v),
+                        ImVec2(u + tw, v + th)
+                    )) {
+                        frames[i].first = !frames[i].first;
+                    }
+
+                    //Disalbe color sets
+                    ImGui::PopStyleColor(2);
+
+                    pos.x += currentTileWidth;
+                    pos.y += currentTileHeight;
+                }
+
+                ImVec2 deltaPos;
+                deltaPos.x = pos.x - startPos.x;
+                deltaPos.y = pos.y - startPos.y;
+
+                ImGui::SetCursorPos(ImVec2(windowPos.x, windowPos.y + deltaPos.y + padding));
+
+                if (ImGui::Button("Start FlipBook")) {
+
+                }
+
+                if (ImGui::Button("Done")) {
+                    showWindow = false;
+                    edittingFlipBook = nullptr;
+                }
+            }
         }
     }
 
