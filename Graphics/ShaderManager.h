e491fa986d009bf3582f22c99c6d651b485edc7b
aliosmansahin <aliosmsahin01@gmail.com>
2025-09-27 22:54:25 +0300

    added some comments to shadermanager.h
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- Graphics/ShaderManager.h
+++ Graphics/ShaderManager.h
@@ -15,34 +15,48 @@
 #include "Shader.h"
 #include "utils/Utils.h"
 
+/*
+	Creates all necessary shaders and handles them
+*/
 class ShaderManager
 {
 public:
 	//main funcs
 	GRAPHICS_API bool			   InitShaderManager();
+	GRAPHICS_API void			   ReleaseShaderManager();
+
+	//shaders
 	GRAPHICS_API bool			   InitShaders(Utils::ShaderType shaderType);
 	GRAPHICS_API void			   UseShaders(Utils::ShaderType shaderType);
 	GRAPHICS_API Utils::ShaderType GetCurrentType();
+
+	//appliers
 	GRAPHICS_API void			   ApplyUniformVec3(const char* uniformName, glm::vec3 vec);
 	GRAPHICS_API void			   ApplyUniformInt(const char* uniformName, int value);
 	GRAPHICS_API void			   ApplyUniformFloat(const char* uniformName, float value);
 	GRAPHICS_API void			   ApplyUniformMatrix(const char* uniformName, glm::mat4 mat);
 	GRAPHICS_API void			   ApplyUniformBool(const char* uniformName, bool value);
 	GRAPHICS_API void			   ApplyTexture(const char* uniformName);
+
+	//updaters
 	GRAPHICS_API void			   UpdateTransformMatrix2D(int windowWidth, int windowHeight, int cameraX, int cameraY, bool invertY = false);
 	GRAPHICS_API void			   UpdateTransformMatrix3D(glm::vec3 eye, int windowWidth, int windowHeight, float cameraX, float cameraY, float cameraZ, float fov);
+
+	//calculators
 	GRAPHICS_API glm::mat4		   GetProjectionMatrix2D(int windowWidth, int windowHeight, bool invertY = false);
-	GRAPHICS_API void			   ReleaseShaderManager();
+
 	//getter for the instance of class
 	GRAPHICS_API static ShaderManager& GetInstance();
+
 private:
 	ShaderManager() = default;
 	~ShaderManager() = default;
 	ShaderManager(const ShaderManager&) = delete;
 	ShaderManager& operator=(const ShaderManager&) = delete;
+
 private:
-	std::map<int, unsigned int> programs;
-	unsigned int currentProgram;
-	Utils::ShaderType currentType = Utils::SHADER_3D;
+	std::map<int, unsigned int> programs; //All shader programs
+	unsigned int currentProgram; //Stores current program
+	Utils::ShaderType currentType = Utils::SHADER_3D; //Stores type of the current shaders
 };
 
